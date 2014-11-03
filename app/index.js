30dbf4be400c7646cddd86932957baffa2d69eee
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-11-03 02:17:08 -0500

    Moving off of typescript. Used yeoman's generator-generator for this.  Does NOTHING different yet, just ran it.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,54 @@
-/// <reference path="../typings/tsd.d.ts" />
-var generators;
-generators = require('yeoman-generator');
-module.exports = generators.Base.extend({
-    // The name `constructor` is important here
-    constructor: function () {
-        // Calling the super constructor is important so our generator is correctly setup
-        generators.Base.apply(this, arguments);
-        // And next add your custom code
-        this.option('coffee'); // This method adds support for a `--coffee` flag
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+var PurecssAngularGenerator = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the super-duper PurecssAngular generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.dest.mkdir('app');
+      this.dest.mkdir('app/templates');
+
+      this.src.copy('_package.json', 'package.json');
+      this.src.copy('_bower.json', 'bower.json');
+    },
+
+    projectfiles: function () {
+      this.src.copy('editorconfig', '.editorconfig');
+      this.src.copy('jshintrc', '.jshintrc');
     }
+  },
+
+  end: function () {
+    this.installDependencies();
+  }
 });
+
+module.exports = PurecssAngularGenerator;
